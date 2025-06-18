a4e29715514c595a9312e3fe3222ee32d50b5f36
DSV230123 <kimbriiezyk@gmail.com>
2025-06-18 16:47:43 -0600

    mapa funciona correctamente

--- src/js/mapas/index.js
+++ src/js/mapas/index.js
@@ -1,12 +1,12 @@
 import L from 'leaflet';
 
 // Coordenadas de la Brigada de Comunicaciones del Ejército
-const map = L.map('map').setView([14.61468, -90.51403], 16);
+const map = L.map('map').setView([14.575684, -90.533469], 16);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors',
 }).addTo(map);
 
 L.marker([14.575684, -90.533469]).addTo(map)
-.bindPopup('Brigada de Comunicaciones del Ejército de Guatemala')
+.bindPopup('Area de entrenamiento BCE')
 .openPopup();
